e6101c664b7feea1e0bc3e471c4f7f2f752a71e5
Lucas <imcvlucas@gmail.com>
2023-08-02 16:58:13 -0400

    minor changes to settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['www.trackmycar.xyz','www.cvlucastorres.com', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','www.cvlucastorres.com', '.vercel.app']
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
@@ -108,21 +108,6 @@ DATABASES = {
 }
 
 password = 'aTtsngYFTZZ2sbkCknCc'
-
-# digitalocean app platform database
-# if DEVELOPMENT_MODE is True:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         }
-#     }
-# elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-#     if os.getenv("DATABASE_URL", None) is None:
-#         raise Exception("DATABASE_URL environment variable not defined")
-#     DATABASES = {
-#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
