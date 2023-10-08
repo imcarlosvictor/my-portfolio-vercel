a076b63e4d38674ad499a88a855d14e137823291
Lucas <imcvlucas@gmail.com>
2023-10-08 07:15:06 -0400

    changes to name, color, and favicon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','www.cvlucastorres.com', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','www.imcarlosvictor.com', '.vercel.app']
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
@@ -39,7 +39,7 @@ DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'imcvlucas@gmail.com'
+EMAIL_HOST_USER = 'imcarlosvictor@gmail.com'
 EMAIL_HOST_PASSWORD = 'svrfznvfvlauwlke'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
